7e142beea000f4773ac0022ddf5e69ba1aa8c678
amosoni <13860392700@163.com>
2025-07-08 20:48:03 +0800

    fix: comment out next-intl getTranslations in sign-in metadata to avoid namespace fallback error

--- app/[locale]/(auth)/(center)/sign-in/[[...sign-in]]/page.tsx
+++ app/[locale]/(auth)/(center)/sign-in/[[...sign-in]]/page.tsx
@@ -1,23 +1,23 @@
 import { SignIn } from '@clerk/nextjs';
-import { getTranslations, setRequestLocale } from 'next-intl/server';
+import { setRequestLocale } from 'next-intl/server';
 import { getI18nPath } from '@/utils/Helpers';
 
 type ISignInPageProps = {
   params: Promise<{ locale: string }>;
 };
 
-export async function generateMetadata(props: ISignInPageProps) {
-  const { locale } = await props.params;
-  const t = await getTranslations({
-    locale,
-    namespace: 'SignIn',
-  });
-
-  return {
-    title: t('meta_title'),
-    description: t('meta_description'),
-  };
-}
+// export async function generateMetadata(props: ISignInPageProps) {
+//   const { locale } = await props.params;
+//   const t = await getTranslations({
+//     locale,
+//     namespace: 'SignIn',
+//   });
+//
+//   return {
+//     title: t('meta_title'),
+//     description: t('meta_description'),
+//   };
+// }
 
 export default async function SignInPage(props: ISignInPageProps) {
   const { locale } = await props.params;
